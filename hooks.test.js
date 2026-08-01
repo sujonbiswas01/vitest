d17dc69687deeabb6ddd7bbbe9269777ee4133c5
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-08-01 16:24:37 +0600

    mock spies

--- hooks.test.js
+++ hooks.test.js
@@ -1,31 +1,43 @@
-import {it,expect,beforeAll,afterAll,beforeEach,afterEach} from "vitest"
+import { it, expect, beforeAll, afterAll, beforeEach, afterEach, describe } from "vitest"
 import { User } from "./hooks";
- const testEmail="test@test.com"
+const testEmail = "test@test.com"
 let user;
 
-beforeEach(()=>{
-    user=new User(testEmail)
+beforeEach(() => {
+    user = new User(testEmail)
 })
-it("should update the email",()=>{
-   
+
+describe("test", () => {
+    
+    it("should update the email", () => {
+
+        const newTestEmail = "newTest@test.com"
+        user.updateEmail(newTestEmail)
+        expect(user.getEmail()).toBe(newTestEmail)
+    })
+
+    it("should get the email", () => {
+        expect(user.getEmail()).toBe(testEmail)
+    })
+
+    it("should delete the email", () => {
+        user.deleteEmail()
+        expect(user.getEmail()).toBe(null)
+    })
+
+    it("should get the email length", () => {
+        expect(user.getEmailLength()).toBe(testEmail.length)
+    })
+
+    it("should get the email capitalized", () => {
+        expect(user.getEmailCapitalized()).toBe(testEmail.toUpperCase())
+    })
+
+
+})
+
+it.concurrent("should update the email concurrently",()=>{
     const newTestEmail="newTest@test.com"
     user.updateEmail(newTestEmail)
     expect(user.getEmail()).toBe(newTestEmail)
-})
-
-it("should get the email",()=>{
-    expect(user.getEmail()).toBe(testEmail)
-})
-
-it("should delete the email",()=>{
-    user.deleteEmail()
-    expect(user.getEmail()).toBe(null)
-})
-
-it("should get the email length",()=>{
-    expect(user.getEmailLength()).toBe(testEmail.length)
-})
-
-it("should get the email capitalized",()=>{
-    expect(user.getEmailCapitalized()).toBe(testEmail.toUpperCase())
 })
