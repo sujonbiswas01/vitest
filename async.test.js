037fad460cae92c75de27b9e39842cc6ec7cf450
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-08-01 14:18:19 +0600

    hook

--- async.test.js
+++ async.test.js
@@ -1,5 +1,18 @@
-import {it,expect} from "vitest"
+import {it,expect,beforeAll,afterAll,beforeEach,afterEach} from "vitest"
+import CryptoJS from "crypto-js";
 import { encrypMessage, encrypMessagePromize } from "./async";
+beforeAll(()=>{
+    console.log("before all")
+})
+afterAll(()=>{
+    console.log("after all")
+})
+beforeEach(()=>{
+    console.log("before each")
+})
+afterEach(()=>{
+    console.log("after each")
+})
 it("should encrypt a message",async()=>{
     const message="Programming Hero"
     const secretKey="458963"
@@ -14,4 +27,13 @@ it("should also just pass",async()=>{
     const secretKey="458963"
     const encryptedData= await encrypMessagePromize(message,secretKey,(message)=>{resolve(message)})
     expect(encryptedData).toBeDefined()
+})
+
+it("should also just pass",async()=>{
+    const message="Programming Hero"
+    const secretKey="458963"
+    const encryptedData= await encrypMessagePromize(message,secretKey,(message)=>{resolve(message)})
+    expect(encryptedData).toBeDefined()
+    const decryptedData=CryptoJS.AES.decrypt(encryptedData,secretKey).toString(CryptoJS.enc.Utf8)
+    expect(decryptedData).toBe(message)
 })
